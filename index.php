18007c3af59cb938de8fc2570f1d89c48404aaf9
jcitdeveloper <amalathassenthuran@gmail.com>
2019-09-29 18:08:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if(isset($_POST['signup'])){
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Online Exam Portal - Endeavor</title>
+		<title>Online Exam Portal -JC DEVELOPER</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 	</head>
